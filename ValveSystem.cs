f4c184b54a8e1772b3b218a6987478295c65cdec
Nathan <nathanvd@telenet.be>
2021-06-30 20:59:13 +0200

    push not recognized...

---
+++ ValveSystem.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValveSystem : MonoBehaviour
+{
+    Controller controls;
+    private float pusht;
+    public float scale = 1f;
+    private bool hand = false;
+    private float Scalefactor;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        controls = new Controller();
+
+        controls.Gameplay.Grasp.performed += ctx => pusht = ctx.ReadValue<float>();
+        controls.Gameplay.Grasp.canceled += ctx => pusht = 0f;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hand == true)
+        {
+            Scalefactor = 1f - (1f+0.65f)*pusht*scale;
+            transform.localScale = new Vector3(transform.localScale.x*Scalefactor, transform.localScale.y, transform.localScale.z*Scalefactor);
+            
+        }
+        Debug.Log(pusht);
+    }
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.name == "Hand")
+        {
+            hand = true;
+        }
+        else
+        {
+            hand = false;
+        }
+    }
+}
