da828051be1db2b845980b7ec8827467006c12d8
Nathan <nathanvd@telenet.be>
2021-07-12 04:34:15 +0200

    close to being finished

--- GlassFill.cs
+++ GlassFill.cs
@@ -4,24 +4,57 @@ using UnityEngine;
 
 public class GlassFill : MonoBehaviour
 {
-    private float counter = 1.9f;
+    public float FillStart;
+    public float FillEnd;
     private Renderer rend;
     [SerializeField]
     private ValveSystem FluidParameters;
     private float DropScale;
     public float FillSpeed;
+    public GameObject Glass;
+    MeshRenderer GlassMesh;
+    public GameObject Full;
+    MeshRenderer FluidMesh;
+    MeshCollider FluidCollider;
+    private float Spawn;
+    public float SpawnChance;
+    private bool GlassPresent = true;
+    private float counter;
 
     // Start is called before the first frame update
     void Start()
     {
         rend = gameObject.GetComponent<Renderer>();
         rend.material.shader = Shader.Find("Shader Graphs/FluidFiller");
+        counter = FillStart;
+        GlassMesh = Glass.GetComponent<MeshRenderer>();
+        FluidMesh = gameObject.GetComponent<MeshRenderer>();
+        FluidCollider = gameObject.GetComponent<MeshCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
         DropScale = FluidParameters.DropScale;
+        if (rend.material.GetFloat("Vector1_3EA5C93") > FillEnd)
+        {
+            Full.SetActive(true);
+            GlassMesh.enabled = false;
+            FluidMesh.enabled = false;
+            FluidCollider.enabled = false;
+            rend.material.SetFloat("Vector1_3EA5C93", FillStart);
+            GlassPresent = false;
+        }
+        Spawn = Random.Range(0f, 1f);
+        if (Spawn > 1 - SpawnChance && GlassPresent == false)
+        {
+            counter = FillStart;
+            Full.SetActive(false);
+            GlassMesh.enabled = true;
+            FluidMesh.enabled = true;
+            FluidCollider.enabled = true;
+            GlassPresent = true;
+        }
     }
     void OnTriggerEnter(Collider col)
     {
